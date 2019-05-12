ed8519ecc8c744253ec8dfdbd0bfb47b78bd66e1
Aidar <brisingr@inbox.ru>
2019-05-12 18:22:06 +0600

    Initial Commit

---
+++ whoAreYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whoAreYou
+//
+//  Created by Мак on 5/12/19.
+//  Copyright © 2019 Aidar Zhussupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
